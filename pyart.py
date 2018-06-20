695ce06391c87f70596715491dfe66b7df59ccf1
Tom <von@disroot.org>
2018-06-20 13:01:08 +0200

    Add new character lists

--- pyart.py
+++ pyart.py
@@ -1,4 +1,5 @@
 #! /usr/bin/python3
+# -*- coding: utf-8 -*-
 
 import random
 import sys
@@ -6,9 +7,12 @@ import sys
 chars1 = ["|","-","_","=","/","\\","#"," "]
 chars2 = ["(",")","{","}","[","]"]
 chars3 = ["|","_"," "]
+chars6 = ["ᚐ","ᚑ","ᚒ","ᚓ","ᚔ"]
+chars7 = ["ᚋ","ᚌ","ᚍ","ᚎ","ᚏ"]
+chars8 = ["▖","▗","▘","▙","▚","▛","▜","▝","▞","▟"]
 
 if len(sys.argv) < 2:
-    sys.argv = ['pyart.py',random.randrange(0, 4)]
+    sys.argv = ['pyart.py',random.randrange(0, 9)]
 
 sys.argv[1] = int(sys.argv[1])
 
@@ -16,6 +20,16 @@ if sys.argv[1] == 1:
     chars = chars1
 elif sys.argv[1] == 2:
     chars = chars2
+elif sys.argv[1] == 4:
+    chars = chars4
+elif sys.argv[1] == 5:
+    chars = chars5
+elif sys.argv[1] == 6:
+    chars = chars6
+elif sys.argv[1] == 7:
+    chars = chars7
+elif sys.argv[1] == 8:
+    chars = chars8
 else:
     chars = chars3
 
